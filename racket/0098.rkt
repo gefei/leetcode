5990e97aeb96c3fce98e863b1e2a2eb380199d7b
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-01-02 13:22:27 +0100

    0098 racket

---
+++ racket/0098.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(struct tree-node
+  (val left right) #:mutable #:transparent)
+(define (make-tree-node [val 0])
+  (tree-node val #f #f))
+
+(define/contract (is-valid-bst root)
+  (-> (or/c tree-node? #f) boolean?)
+  (define (rec root lo hi parent)
+      ;(println (list lo hi))
+      ;(println (list root lo hi parent))
+      (cond
+          [(not root) #t]
+          [(<= (tree-node-val root) lo) #f]
+          [(>= (tree-node-val root) hi) #f]
+          [else 
+              (and
+               (rec (tree-node-left root) lo (min (tree-node-val root) hi) root)
+               (rec (tree-node-right root) (max (tree-node-val root) lo) hi root)
+              )
+          ]
+      )
+  )
+  (rec root (- (- (expt 2 31)) 1) (expt 2 31) #f)
+)
+
+;(define x (make-tree-node 1)) 
+(define x 
+     (tree-node 2 
+     (make-tree-node 1) (make-tree-node 3))) 
+(is-valid-bst x)
+(define y 
+     (tree-node 2 
+     (make-tree-node 3) (make-tree-node 1))) 
+(is-valid-bst y)
+(define z 
+     (tree-node 5 
+     (make-tree-node 1)
+     (tree-node 4 (make-tree-node 3) (make-tree-node 6))))
+(is-valid-bst z)
