4b379b7b2a15b500da34eb46cc2e88734020d279
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-05-12 20:08:05 +0200

    2785 elixir

---
+++ elixir/2785.ex
@@ -0,0 +1,33 @@
+defmodule Solution do
+  @spec sort_vowels(s :: String.t) :: String.t
+  def sort_vowels(s) do
+    lst = String.to_charlist(s)
+    vowels = lst |> Enum.filter(&(is_vowel(&1))) |> Enum.sort()
+    groups = lst |> Enum.chunk_by(&(is_vowel(&1)))
+    rec(groups, vowels, "")
+    # |> Enum.reverse()
+    #|> Enum.join()
+  end
+
+  def rec([], _, acc), do: acc
+  def rec([h|t], vowels, acc) do
+    cond do
+      is_vowel(Enum.at(h, 0)) -> true
+          len = length(h)
+          rest_vowels = Enum.drop(vowels, len)
+          prefix = Enum.take(vowels, len) |> Enum.map(&(<<&1>>)) |> Enum.join()
+          rec(t, rest_vowels,  acc <> prefix)
+      true -> rec(t, vowels, acc <> List.to_string(h))
+    end
+  end
+
+  def is_vowel(c) do
+    (c == ?A || c == ?a)
+      || (c == ?E || c == ?e)
+      || (c == ?I || c == ?i)
+      || (c == ?O || c == ?o)
+      || (c == ?U || c == ?u)
+  end
+end
+
+Solution.sort_vowels(("lEetcOde")) |> IO.inspect()
