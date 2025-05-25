7111ec0836fa8a86546abb6cf4348b2357caf946
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-05-25 13:43:16 +0200

    0096 elixir

---
+++ elixir/0096.ex
@@ -0,0 +1,50 @@
+# var numTrees = function(n) {
+#     const res = {0:1, 1:1}
+#     for (let i = 2; i <= n; i++) {
+#         res[i] = 0
+#         for (let j = 0; j <= i>>1; j++) {
+#             const k = i - j - 1
+#             if (j > k) break
+#             if (j === k) res[i] = res[i] + res[j] * res[k];
+#             else         res[i] = res[i] + res[j] * res[k] * 2;
+#         }
+#     }
+#     return res[n]
+# };
+
+defmodule Solution do
+  @spec num_trees(n :: integer) :: integer
+  def num_trees(n) do
+    cond do
+      n == 0 -> 1
+      n == 1 -> 1
+      true ->
+        m = %{0=>1, 1=>1}
+        t = map(2, n, m)
+        Map.get(t, n)
+    end
+  end
+
+  def map(i, n, m) when i > n, do: m
+  def map(i, n, m) do
+    this_one = loop(0, i, m, 0)
+    new_m = Map.put(m, i, this_one)
+    {:old, m, :new, new_m}
+    map(i+1, n, new_m)
+  end
+
+  def loop(cur, i, map, acc) do
+    k = i - cur - 1
+    {:i, i, :cur, cur, :k, k, :acc, acc}
+    cond do
+      cur > k -> acc
+      cur == k -> Map.get(map, k) * Map.get(map, cur) + acc
+      true -> loop(cur+1, i, map, acc+Map.get(map, k)*Map.get(map, cur)*2)
+    end
+  end
+end
+
+Solution.num_trees(3) |> IO.inspect()
+Solution.num_trees(4) |> IO.inspect()
+Solution.num_trees(5) |> IO.inspect()
+Solution.num_trees(6) |> IO.inspect()
