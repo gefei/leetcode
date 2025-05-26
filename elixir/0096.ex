c175bbbeb860f8d44e01fae102ed99ab9c1ae6c8
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-05-26 05:18:55 +0200

    cleanup 0096 elixir

--- elixir/0096.ex
+++ elixir/0096.ex
@@ -1,17 +1,3 @@
-# var numTrees = function(n) {
-#     const res = {0:1, 1:1}
-#     for (let i = 2; i <= n; i++) {
-#         res[i] = 0
-#         for (let j = 0; j <= i>>1; j++) {
-#             const k = i - j - 1
-#             if (j > k) break
-#             if (j === k) res[i] = res[i] + res[j] * res[k];
-#             else         res[i] = res[i] + res[j] * res[k] * 2;
-#         }
-#     }
-#     return res[n]
-# };
-
 defmodule Solution do
   @spec num_trees(n :: integer) :: integer
   def num_trees(n) do
