f3e856ba0d1b1562b5b12207cd8d8deab4b42b6d
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-06-20 00:04:29 +0200

    2928 javascript

---
+++ javascript/2928.js
@@ -0,0 +1,25 @@
+var distributeCandies = function(n, limit) {
+    let a = []
+    for (let i = 0; i <= n; i++) {
+        if (i <= limit) {
+            a.push(1)
+        } else {
+            a.push(0)
+        }
+    }
+    for (let k = 2; k <= 3; k++) {
+        let b = []
+        for (let i = 0; i <= n; i++) {
+            let c = 0
+            for (let p = 0; p <= limit && p <= i; p++) {
+                c = c + a[i-p]
+            }
+            b.push(c)
+        }
+        a = b
+    }
+    return a[a.length-1]
+};
+
+console.log(distributeCandies(5, 2))
+console.log(distributeCandies(3, 3))
