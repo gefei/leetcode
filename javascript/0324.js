77fea0a4d64130c5f194834dd45d56d0f5a2d12c
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-06-30 21:35:05 +0200

    0324 javascript

---
+++ javascript/0324.js
@@ -0,0 +1,16 @@
+var wiggleSort = function(nums) {
+    const len = nums.length
+    if (len === 1) return nums
+    nums.sort((a,b) => a-b)
+    const mid = (len >> 1)
+    let a = mid - ((len&1)?0:1), b = len-1
+    const res = []
+    while (a >= 0) {
+        res.push(nums[a--])
+        res.push(nums[b--])
+    }
+    for (let i = 0; i < len; i++) {
+        nums[i] = res[i]
+    }
+    return nums
+};
