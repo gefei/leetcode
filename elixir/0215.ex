5656735e3f81f1f1db38c3f2a4b0059f72fab828
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-06-30 16:14:26 +0200

    0215 elixir

---
+++ elixir/0215.ex
@@ -0,0 +1,16 @@
+defmodule Solution do
+  @spec find_kth_largest(nums :: [integer], k :: integer) :: integer
+  def find_kth_largest(nums, k) do
+    h = nums |> Enum.uniq() |> Enum.random()
+    head = nums |> Enum.filter(&(&1 < h))
+    tail = nums |> Enum.filter(&(&1 >= h))
+    len = length(tail)
+    #IO.inspect({:head, head, :tail, tail})
+    cond do
+      Enum.all?(nums, &(&1==h)) -> h
+      length(nums) == 1 -> hd nums
+      len >= k -> find_kth_largest(tail, k)
+      len <  k -> find_kth_largest(head, k-len)
+    end
+  end
+end
