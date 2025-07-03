14f101d3ad24744d55a785700941486ec6d5807f
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-07-03 17:57:49 +0200

    0848 javascript

---
+++ javascript/0848.js
@@ -0,0 +1,20 @@
+var shiftingLetters = function(s, shifts) {
+    const a = 'a'.charCodeAt(0)
+    shifts = shifts.map(x => x % 26)
+    let sum = 0
+    const prefix = Array(shifts.length)
+    for (let i = shifts.length-1; i >= 0; i--) {
+        sum = (sum + shifts[i]) % 26
+        prefix[i] = sum
+    }
+    console.log(prefix)
+    let res = ''
+    for (let i = 0; i < shifts.length; i++) {
+        const y = s.charCodeAt(i)
+        const p = prefix[i]
+        const x = (y - a + p)  % 26
+        const code = x + a
+        res = res + (String.fromCodePoint(code))
+    }
+    return res
+};
