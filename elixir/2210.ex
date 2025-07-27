0aa6c2e1fd4264ebeec4772bd4b4b1bf30800c44
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-07-27 12:49:21 +0200

    clean up 2210 elixir

--- elixir/2210.ex
+++ elixir/2210.ex
@@ -4,24 +4,23 @@ defmodule Solution do
     step((hd nums), (tl nums), false, false, 0, 0)
   end
 
-  def step(cur, [], _is_asc, _is_dsc, num_hill, num_valey) do
-    num_hill + num_valey
+  def step(cur, [], _is_asc, _is_dsc, num_hill, num_valley) do
+    num_hill + num_valley
   end
-  def step(cur, [h|t], is_asc, is_dsc, num_hill, num_valey) do
-    {:cur, cur, :h, h, :asc,is_asc, :dsc, is_dsc, :h, num_hill, :v, num_valey} |> IO.inspect()
+  def step(cur, [h|t], is_asc, is_dsc, num_hill, num_valley) do
     cond do
-      h == cur -> step(cur, t, is_asc, is_dsc, num_hill, num_valey)
+      h == cur -> step(cur, t, is_asc, is_dsc, num_hill, num_valley)
       h > cur -> cond do
-        is_asc -> step(h, t, true, false, num_hill, num_valey)
+        is_asc -> step(h, t, true, false, num_hill, num_valley)
         is_dsc ->
-          step(h, t, true, false, num_hill, num_valey+1)
-        true -> step(h, t, true, false, num_hill, num_valey)
+          step(h, t, true, false, num_hill, num_valley+1)
+        true -> step(h, t, true, false, num_hill, num_valley)
       end
       h < cur -> cond do
         is_asc ->
-          step(h, t, false, true, num_hill+1, num_valey)
-        is_dsc -> step(h, t, false, true, num_hill, num_valey)
-        true -> step(h, t, false, true, num_hill, num_valey)
+          step(h, t, false, true, num_hill+1, num_valley)
+        is_dsc -> step(h, t, false, true, num_hill, num_valley)
+        true -> step(h, t, false, true, num_hill, num_valley)
       end
     end
   end
