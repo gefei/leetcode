188237f70b76e29378a2cd29803009fe1b874ca4
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-08-21 14:29:54 +0200

    1277/1504 javascript

---
+++ javascript/1277.js
@@ -0,0 +1,35 @@
+var countSquares = function(matrix) {
+    const sum_mat = []
+    for (let arr of matrix) {
+        const row = []
+        let s = 0
+        for (let t of arr) {
+            s = s + t
+            row.push(s)
+        }
+        sum_mat.push(row)
+    }
+    for (let i = 1; i < sum_mat.length; i++) {
+        for (let j = 0; j < sum_mat[i].length; j++) {
+            sum_mat[i][j] = sum_mat[i][j] + sum_mat[i-1][j]
+        }
+    }
+    let s = 0
+    for (let y = 0; y < sum_mat.length; y++) {
+        for (let x = 0; x < sum_mat[y].length; x++) {
+            if (matrix[y][x] === 0) continue
+            for (let y1 = y; y1 < sum_mat.length; y1++) {
+                    const x1 = x + (y1-y)
+                    const up = (y >= 1) ? sum_mat[y-1][x1] : 0
+                    const left = (x >= 1) ? sum_mat[y1][x-1] : 0
+                    const corner = (x>=1 && y>=1) ? sum_mat[y-1][x-1] : 0
+                    if ((y1-y+1) * (x1-x+1) === sum_mat[y1][x1] - up - left + corner) {
+                        s = s + 1
+                    } else {
+                        break
+                    }
+            }
+        }
+    }
+    return s
+};
