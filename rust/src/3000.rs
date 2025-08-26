6f303237bc3438bf4476ad3b8d3ae63dd44d317e
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-08-26 06:36:24 +0200

    3000 rust, update

--- rust/src/3000.rs
+++ rust/src/3000.rs
@@ -1,23 +1,24 @@
 pub fn area_of_max_diagonal(dimensions: Vec<Vec<i32>>) -> i32 {
-    let mut max_diag: i32 = 0;
-    for row in dimensions.clone() {
-        let x = row[0];
-        let y = row[1];
-        let sqr= x*x+y*y;
-        if max_diag <  sqr {
-            max_diag = sqr;
-        }
-    }
-    let mut max_area: i32 = 0;
-    for row in dimensions {
-        let x = row[0];
-        let y = row[1];
-        let sqr= x*x+y*y;
-        if max_diag ==  sqr {
-            if max_area < x*y {
-                max_area = x*y
+        let d: &Vec<Vec<i32>> = &dimensions;
+        let mut max_diag: i32 = 0;
+        for row in d {
+            let x = row[0];
+            let y = row[1];
+            let sqr= x*x+y*y;
+            if max_diag <  sqr {
+                max_diag = sqr;
             }
         }
+        let mut max_area: i32 = 0;
+        for row in d{
+            let x = row[0];
+            let y = row[1];
+            let sqr= x*x+y*y;
+            if max_diag ==  sqr {
+                if max_area < x*y {
+                    max_area = x*y
+                }
+            }
+        }
+        return max_area;
     }
-    return max_area;
-}
