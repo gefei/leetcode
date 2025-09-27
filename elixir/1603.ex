a446e63901986b51b55e7df3e3b8c2b69c3df84f
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2025-09-27 04:52:56 +0200

    1603 elixir

---
+++ elixir/1603.ex
@@ -0,0 +1,24 @@
+defmodule Solution do
+  @spec check_arithmetic_subarrays(nums :: [integer], l :: [integer], r :: [integer]) :: [boolean]
+  def check_arithmetic_subarrays(nums, l, r) do
+    step(nums, l, r, [])
+  end
+
+  def step(nums, [], [], acc), do: acc |> Enum.reverse()
+  def step(nums, [a|t1], [b|t2], acc) do
+    [a,b|t] = slice = nums |> Enum.drop(a) |> Enum.take(b-a+1) |> Enum.sort()
+    slice |> IO.inspect(charlists: :as_list)
+    step(nums, t1, t2, [is_arithmetic(slice, b-a) | acc])
+  end
+
+  def is_arithmetic([a,b], d), do: b-a == d
+  def is_arithmetic([h,h1|t], d) do
+    cond do
+      h1 - h == d -> is_arithmetic([h1|t], d)
+      true -> false
+    end
+  end
+end
+
+Solution.check_arithmetic_subarrays([4,6,5,9,3,7], l = [0,0,2], r = [2,3,5]) |> IO.inspect()
+Solution.check_arithmetic_subarrays([-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], [0,1,6,4,8,7], [4,4,9,7,9,10]) |> IO.inspect()
