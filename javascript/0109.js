fe14e81aaf94217bc0a88be84729ba126f15f6c6
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2026-06-22 20:26:26 +0200

    0109 javascript

---
+++ javascript/0109.js
@@ -0,0 +1,33 @@
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+  
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+var sortedListToBST = function(head) {
+  const arr = []
+  while (head != null) {
+    arr.push(head.val)
+    head = head.next
+  }
+  return sortedArrayToBST(arr)
+};
+
+var sortedArrayToBST = function(nums) {
+  function toBSTRec(nums, lo, hi)  {
+    if (lo > hi) return null
+    if (lo === hi) return new TreeNode(nums[lo])
+    const mid = parseInt((lo + hi) / 2)
+    return new TreeNode(
+        nums[mid],
+        toBSTRec(nums, lo, mid-1),
+        toBSTRec(nums, mid+1, hi),
+    )
+  }
+  return toBSTRec(nums, 0, nums.length-1)
+};
